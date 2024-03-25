03b9d7e8070af88a1d532646bff47807652650b9
[dastaeren] <[03230051.gcbs@rub.edu.bt]>
2024-03-25 12:43:46 -0800

    added code

---
+++ linear_search.py
@@ -0,0 +1,14 @@
+def linear_search(arr, target):
+    for i in range(len(arr)):
+      if arr[i] == target:
+         return i
+    return -1
+    
+arr = [10, 15, 20, 5, 8, 30]
+target = 30
+
+result = linear_search(arr, target)
+if result != -1:
+   print("Element found at index",result)
+else:
+   print("Element not found")      
